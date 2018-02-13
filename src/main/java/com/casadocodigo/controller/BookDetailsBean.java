454a0898721ab18c2d8fc682904c8d09cebfaeaa
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2018-02-13 10:52:15 -0300

    Importando a página de detalhes do livro da casa do código

---
+++ src/main/java/com/casadocodigo/controller/BookDetailsBean.java
@@ -0,0 +1,37 @@
+package com.casadocodigo.controller;
+
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+
+import com.casadocodigo.dao.BookDAO;
+import com.casadocodigo.model.Book;
+
+@Model
+public class BookDetailsBean {
+
+	@Inject
+	private BookDAO bookDAO;
+	
+	private Book book;
+	
+	private Integer id;
+	
+	
+	public void loadBook() {
+		book = bookDAO.findOne(id);
+		System.out.println(book);
+	}
+
+	public Book getBook() {
+		return book;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+}
--- src/main/java/com/casadocodigo/dao/BookDAO.java
+++ src/main/java/com/casadocodigo/dao/BookDAO.java
@@ -15,6 +15,16 @@ public class BookDAO {
 	public void save(Book book) {
 		manager.merge(book);
 	}
+	
+	public Book findOne(Integer bookID) {
+		try {
+		return manager.createQuery("select b from Book b join fetch b.authors where b.id = :bookID", Book.class)
+				.setParameter("bookID", bookID)
+				.getSingleResult();
+		} catch (Exception e) {
+			return null;
+		}
+	}
 
 	public List<Book> findAll() {
 		return manager.createQuery("select distinct(b) from Book b join fetch b.authors", Book.class)
